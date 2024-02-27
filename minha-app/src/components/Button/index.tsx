7448cac54506c5f1c85eef0ec3bbaaba447545bb
Dara Maria Rodrigues Soares <dara.soares@fitbank.com.br>
2024-02-27 14:20:32 -0300

    feat: projeto spacey

---
+++ minha-app/src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { ButtonComponent } from "./styles";
+
+export interface ButtonProps {
+  text?: string;
+  fullWidth?: boolean;
+}
+
+function Button({ text, fullWidth }: ButtonProps) {
+  return (
+    <>
+      <ButtonComponent fullWidth={fullWidth}>{text}</ButtonComponent>
+    </>
+  );
+}
+
+export default Button;
---
+++ minha-app/src/components/Form/index.tsx
@@ -0,0 +1,19 @@
+import { FormComponent, Label, Input } from "./styles";
+
+export interface FormProps {
+  text: string;
+  type?: "text" | "email" | "number";
+}
+
+function Form({ text }: FormProps) {
+  return (
+    <>
+      <FormComponent>
+        <Label>{text}</Label>
+        <Input />
+      </FormComponent>
+    </>
+  );
+}
+
+export default Form;
